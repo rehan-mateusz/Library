bb7e24aad5b71a9fa1f816a09847cf0e4b41c4ab
kulas971 <rehan.mateusz@gmail.com>
2021-08-19 17:15:21 +0200

    added BooksListAPIView

--- libraryproject/books_rest/views.py
+++ libraryproject/books_rest/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics
+from django_filters import rest_framework as filters
 
-# Create your views here.
+from . import filters
+from . import serializers
+
+from books import models
+
+class BooksListAPIView(generics.ListAPIView):
+    queryset = models.Book.objects.all()
+    serializer_class = serializers.BooksSerializer
+    filterset_class = filters.BookFilter

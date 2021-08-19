388bf055b81d9f4c4262e055bc789144f0a6a930
kulas971 <rehan.mateusz@gmail.com>
2021-08-19 14:18:46 +0200

    changed labels for date fields

--- libraryproject/books/filters.py
+++ libraryproject/books/filters.py
@@ -5,11 +5,13 @@ from .languages import languages
 
 class BookFilter(django_filters.FilterSet):
     publication_language = django_filters.ChoiceFilter(choices=languages)
-
+    published_after = django_filters.DateFilter(lookup_expr='gt',
+        label='Published after:', field_name='published_date')
+    published_before = django_filters.DateFilter(lookup_expr='lt',
+        label='Published before:', field_name='published_date')
     class Meta:
         model = models.Book
         fields = {
             'title': ['icontains'],
             'author': ['icontains'],
-            'published_date': ['gt', 'lt'],
         }
